f5ce4f9c08cdff882dc055f69b5610095a6ff3c8
malikbendevv <malikbendevv@gmail.com>
2022-12-04 22:10:21 +0100

    NavBarDone

---
+++ components/Button.jsx
@@ -0,0 +1,11 @@
+const Button = ({ classStyles, btnName, handleClick }) => (
+  <button
+    type="button"
+    onClick={handleClick}
+    className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-popins font-semibold text-white ${classStyles} `}
+  >
+    {btnName}{" "}
+  </button>
+);
+
+export default Button;
